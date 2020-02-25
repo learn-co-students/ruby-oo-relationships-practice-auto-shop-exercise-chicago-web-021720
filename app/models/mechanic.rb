7913a6bddc36d2f4fbe08df0ea5eb8b1310b3054
djoverbeatz29 <jroverby92@gmail.com>
2020-02-25 17:29:27 -0600

    Done.

--- app/models/mechanic.rb
+++ app/models/mechanic.rb
@@ -1,11 +1,29 @@
 class Mechanic
 
-  attr_reader :name, :specialty
+    attr_reader :name, :specialty
 
-def initialize(name, specialty)
-  @name = name
-  @specialty = specialty
-end
+    @@all = []
 
+    def self.all
+        @@all
+    end
+
+    def initialize(name, specialty)
+        @name = name
+        @specialty = specialty
+        @@all << self
+    end
+
+    def cars
+        Car.all.find_all { |car| car.mechanic == self }
+    end
+
+    def owners
+        self.cars.map { |car| car.owner }.uniq
+    end
+
+    def owner_names
+        self.owners.map { |owner| owner.name }
+    end
 
 end
--- config/environment.rb
+++ config/environment.rb
@@ -1,3 +1,9 @@
-require 'bundler/setup'
-Bundler.require
-require_all 'app'
+require 'pry'
+
+# require 'bundler/setup'
+# Bundler.require
+# require_all 'app'
+
+require_relative '../app/models/car_owner.rb'
+require_relative '../app/models/car.rb'
+require_relative '../app/models/mechanic.rb'
